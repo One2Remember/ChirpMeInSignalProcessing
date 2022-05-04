f3a972c36a3db9665b44533c96759168d63b4530
eg11 <eg11@illinois.edu>
2022-05-04 17:10:00 -0500

    added latency support for trimming data before processing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.io import wavfile
 from scipy.stats import linregress
 
+SIGMA = 1.2
 
 def get_slope(file_path, latency_seconds):
     sample_rate, data = wavfile.read(file_path)  # load the data
@@ -12,8 +13,9 @@ def get_slope(file_path, latency_seconds):
     maxFreqIndices = np.argmax(Pxx, axis=1)
     frequencies = freqs[maxFreqIndices]
 
-    # TODO: find latency index in bins
-    chop_off = int(len(bins) * 0.2)
+    # use latency and SIGMA to trim the dataset
+    res = list(filter(lambda i: i > latency_seconds, bins))[0]
+    chop_off = int(bins.tolist().index(res) * SIGMA)
     slope, intercept, r_value, p_value, std_err = linregress(bins[chop_off:-chop_off], frequencies[chop_off:-chop_off])
     return slope
 
@@ -25,7 +27,7 @@ def print_spectrogram(file_path):
     plt.xlabel('Time [sec]')
     plt.show()
 
-def print_max_frequencies(file_path):
+def print_dominant_frequencies(file_path):
     sample_rate, data = wavfile.read(file_path)  # load the data
     a = np.mean(data.T, axis=0)  # convert to mono track
     Pxx, freqs, bins, im = plt.specgram(a, NFFT=1024, Fs=sample_rate)
@@ -38,7 +40,7 @@ def print_max_frequencies(file_path):
 
 if __name__ == '__main__':
     file_path = 'recording_3_clean.wav'
-    print(get_slope(file_path, 0.1))
+    print(get_slope(file_path, 0.2))
     print_spectrogram(file_path)
-    print_max_frequencies(file_path)
+    print_dominant_frequencies(file_path)
 
